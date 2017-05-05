9035380d633157b1d9a02b73e577bae104969c7b
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-05-05 16:52:42 +0400

    Changes:+1:

---
+++ SignalRChat/SignalRChat/SignalRChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+
+namespace SignalRChat
+{
+    public class SignalRChatHub : Hub
+    {
+        public void BroadCastMessage(string msgFrom, string msg)
+        {
+            Clients.All.receiveMessage(msgFrom, msg);
+        }
+    }
+}
---
+++ SignalRChat/SignalRChat/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Add the SignalR Middlewware to the OWIN pipeline
+            app.MapSignalR();
+        }
+    }
+}
+
+
